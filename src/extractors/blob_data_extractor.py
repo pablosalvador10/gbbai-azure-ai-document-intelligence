49e62910977a78b9a35fd5e6a38c5ef1b98cecd4
Pablo Salvador Lopez <pablosal@microsoft.com>
2024-01-04 14:32:46 +0100

    Add first iteration

---
+++ src/extractors/blob_data_extractor.py
@@ -0,0 +1,177 @@
+import io
+import json
+import os
+import pickle
+from typing import Any, List, Dict
+
+from azure.storage.blob import BlobServiceClient
+from docx import Document
+from dotenv import load_dotenv
+
+from src.extractors.pdf_data_extractor import PDFHelper
+from utils.ml_logging import get_logger
+
+logger = get_logger()
+pdf_helper = PDFHelper()
+
+
+class AzureBlobManager:
+    """
+    Class for managing interactions with Azure Blob Storage. It provides functionalities
+    to read and write data to blobs, especially focused on handling various file formats.
+
+    Attributes:
+        container_name (str): Name of the Azure Blob Storage container.
+        service_client (BlobServiceClient): Azure Blob Service Client.
+        container_client: Azure Container Client specific to the container.
+    """
+
+    def __init__(self, container_name: str):
+        """
+        Initialize the AzureBlobManager with a container name.
+
+        Args:
+            container_name (str): Name of the Azure Blob Storage container.
+        """
+        try:
+            load_dotenv()
+            connect_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+            if connect_str is None:
+                logger.error(
+                    "AZURE_STORAGE_CONNECTION_STRING not found in environment variables."
+                )
+                raise EnvironmentError(
+                    "AZURE_STORAGE_CONNECTION_STRING not found in environment variables."
+                )
+            self.service_client = BlobServiceClient.from_connection_string(connect_str)
+            self.container_client = self.service_client.get_container_client(
+                container_name
+            )
+            logger.info(f"Initialized AzureBlobManager with container {container_name}")
+        except Exception as e:
+            logger.error(f"Error initializing AzureBlobManager: {e}")
+            raise
+
+    def change_container(self, new_container_name: str):
+        """
+        Changes the Azure Blob Storage container.
+
+        Args:
+            new_container_name (str): The name of the new container.
+        """
+        self.container_client = self.service_client.get_container_client(
+            new_container_name
+        )
+        logger.info(f"Container changed to {new_container_name}")
+
+    def load_object(self, file_name: str) -> Any:
+        """
+        Loads an object from Azure Blob Storage based on the file extension.
+        Supported file formats are CSV, JSON, Pickle, Parquet, DOCX, PDF, and plain text.
+
+        Args:
+            file_name (str): The name of the file to read from.
+
+        Returns:
+            Any: The object read from the file, typically a Pandas DataFrame for structured data files,
+                or a string for DOCX, PDF, and text files.
+
+        Raises:
+            ValueError: If the file format is unsupported or not recognized.
+        """
+        try:
+            # Extract file format from the file name
+            _, file_extension = os.path.splitext(file_name)
+            file_format = file_extension.lstrip(".").lower()
+
+            blob_client = self.container_client.get_blob_client(file_name)
+            downloader = blob_client.download_blob()
+            content = downloader.readall()
+            if file_format == "json":
+                data = json.loads(content)
+                logger.info(f"Successfully loaded JSON file {file_name}")
+            elif file_format == "pickle":
+                data = pickle.loads(content)
+                logger.info(f"Successfully loaded Pickle file {file_name}")
+            elif file_format == "docx":
+                doc = Document(io.BytesIO(content))
+                data = "\n".join([para.text for para in doc.paragraphs])
+                logger.info(f"Successfully loaded DOCX file {file_name}")
+            elif file_format == "pdf":
+                data = pdf_helper.extract_text_from_pdf_bytes(content)
+                metadata = pdf_helper.extract_metadata_from_pdf_bytes(content)
+                logger.info(f"Successfully loaded PDF file {metadata}")
+            elif file_format == "txt":
+                data = content.decode()
+                logger.info(f"Successfully loaded TXT file {file_name}")
+            else:
+                logger.error(f"Unsupported file format: {file_format}")
+                raise ValueError("Unsupported file format: " + file_format)
+
+            return data
+        except Exception as e:
+            logger.error(f"Error loading object from blob: {e}")
+            raise
+
+    def download_files_to_folder(self, folder_path: str, local_dir: str) -> None:
+        """
+        Downloads all files from a specified folder in Azure Blob Storage to a local directory.
+
+        Args:
+            folder_path (str): The path to the folder within the blob container.
+            local_dir (str): The local directory to which the files will be downloaded.
+        """
+        try:
+            # Ensure folder path ends with a '/'
+            if not folder_path.endswith('/'):
+                folder_path += '/'
+
+            blob_list = self.container_client.list_blobs(name_starts_with=folder_path)
+            for blob in blob_list:
+                local_file_path = os.path.join(local_dir, os.path.basename(blob.name))
+                blob_client = self.container_client.get_blob_client(blob.name)
+                with open(local_file_path, "wb") as file:
+                    downloader = blob_client.download_blob()
+                    file.write(downloader.readall())
+
+                logger.info(f"Downloaded {blob.name} to {local_file_path}")
+
+        except Exception as e:
+            logger.error(f"An error occurred while downloading files: {e}")
+            raise
+
+    def load_files_from_folder(self, folder_path: str) -> List[Dict[str, bytes]]:
+        """
+        Loads all files from a specified folder in Azure Blob Storage.
+
+        Args:
+            folder_path (str): The path to the folder within the blob container.
+        Returns:
+            List[Dict[str, bytes]]: A list of dictionaries where each dictionary has the file name as the key and the file content as the value.
+        """
+        try:
+            # Ensure folder path ends with a '/'
+            if not folder_path.endswith('/'):
+                folder_path += '/'
+
+            # List all blobs in the specified folder
+            blob_list = self.container_client.list_blobs(name_starts_with=folder_path)
+            files = [blob.name for blob in blob_list]
+
+            if not files:
+                logger.info(f"No files found in folder: {folder_path}")
+                return []
+
+            logger.info(f"Found {len(files)} files in {folder_path}")
+
+            # Load each file
+            loaded_files = []
+            for file_name in files:
+                loaded_files.append({file_name: self.load_object(file_name)})
+                logger.info(f"Loaded file: {file_name}")
+
+            return loaded_files
+
+        except Exception as e:
+            logger.error(f"An error occurred while loading files from folder: {e}")
+            raise
---
+++ src/extractors/ocr_data_extractor.py
@@ -0,0 +1,99 @@
+import glob
+import fitz
+import os
+from urllib.parse import urlparse
+import tempfile
+from typing import Optional
+from src.extractors.pdf_data_extractor import PDFHelper
+from src.extractors.blob_data_extractor import AzureBlobManager
+from utils.ml_logging import get_logger
+
+logger = get_logger()
+
+class OCRHelper:
+    """
+    Class for OCR functionalities, particularly extracting images from PDF files.
+    """
+
+    def __init__(self, container_name: Optional[str] = None):
+        """
+        Initialize the OCRHelper with a container name.
+        Args:
+            container_name (str): Name of the Azure Blob Storage container.
+        """
+        self.blob_manager = None
+        if container_name:
+            self.init_blob_manager(container_name)
+
+    def init_blob_manager(self, container_name: str) -> None:
+        """
+        Initialize the AzureBlobManager with a container name.
+        Args:
+            container_name (str): Name of the Azure Blob Storage container.
+        """
+        self.blob_manager = AzureBlobManager(container_name)
+
+    def extract_images_from_pdf(self, input_path: str, output_path: str) -> None:
+        """
+        Extracts pages from a PDF file or a folder of PDF files and saves them as pictures.
+        Args:
+            input_path (str): Path to the PDF file or folder of PDF files.
+            output_path (str): Path to the folder where the pictures will be saved.
+        """
+        is_url = urlparse(input_path).scheme in ['http', 'https']
+
+        if is_url:
+            logger.info(f"Input path is a URL: {input_path}")
+            with tempfile.TemporaryDirectory() as temp_dir:
+                self.blob_manager.download_files_to_folder(input_path, temp_dir)
+                self._process_pdf_path(temp_dir, output_path)
+        else:
+            logger.info(f"Input path is a local file or directory: {input_path}")
+            self._process_pdf_path(input_path, output_path)
+
+    def _process_pdf_path(self, input_path: str, output_path: str) -> None:
+        """
+        Processes a PDF file or all PDF files in a directory.
+        Args:
+            input_path (str): Path to the PDF file or directory containing PDF files.
+            output_path (str): Path to save the extracted images.
+        """
+        if os.path.isdir(input_path):
+            self._process_pdf_directory(input_path, output_path)
+        elif os.path.isfile(input_path) and input_path.lower().endswith('.pdf'):
+            self._process_single_pdf(input_path, output_path)
+        else:
+            logger.error("The input path is neither a valid PDF file nor a directory.")
+
+    def _process_pdf_directory(self, directory_path: str, output_path: str) -> None:
+        """
+        Processes all PDF files in a directory and saves each page as an image.
+        Args:
+            directory_path (str): Directory containing PDF files.
+            output_path (str): Directory where the images will be saved.
+        """
+        all_files = glob.glob(os.path.join(directory_path, "*.pdf"))
+        for file_path in all_files:
+            self._process_single_pdf(file_path, output_path)
+
+    def _process_single_pdf(self, file_path: str, output_path: str) -> None:
+        """
+        Processes a single PDF file and saves each page as an image.
+        Args:
+            file_path (str): Path to the PDF file.
+            output_path (str): Directory where the images will be saved.
+        """
+        zoom_x = 2.0  # horizontal zoom
+        zoom_y = 2.0  # vertical zoom
+        mat = fitz.Matrix(zoom_x, zoom_y)  # zoom factor 2 in each dimension
+
+        logger.info(f"Opening file: {file_path}")
+        doc = fitz.open(file_path)
+        base_filename = os.path.splitext(os.path.basename(file_path))[0]
+
+        for page_number, page in enumerate(doc):
+            logger.info(f"Processing page {page_number + 1} of {file_path}")
+            pix = page.get_pixmap(matrix=mat)
+            output_filename = f"{base_filename}-page-{page_number + 1}.png"
+            pix.save(os.path.join(output_path, output_filename))
+            logger.info(f"Saved image: {os.path.join(output_path, output_filename)}")
